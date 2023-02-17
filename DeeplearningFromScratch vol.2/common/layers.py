1881ff8b43c632bb46d094972b704f2c1176aedc
jungnerd <hello_984@naver.com>
2023-02-17 21:06:51 +0900

    230217 update

--- DeeplearningFromScratch vol.2/common/layers.py
+++ DeeplearningFromScratch vol.2/common/layers.py
@@ -70,4 +70,27 @@ class Affine: # TODO : MatMul class를 이용해서 구현하기
 
 class SoftmaxWithLoss:
     def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None
+        self.t = None
         
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        
+        # 정답 레이블이 원핫 벡터인 경우 정답의 인덱스로 변환
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+            
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+        
+        return dx
